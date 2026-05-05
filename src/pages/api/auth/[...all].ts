d64cc19a2f947157fb7f8f7cc9f70624bccdd226
CastDev-j <23031429@itcelaya.edu.mx>
2026-05-04 20:31:57 -0600

    removed unused async function

--- src/pages/api/auth/[...all].ts
+++ src/pages/api/auth/[...all].ts
@@ -1,6 +1,6 @@
 import { auth } from "@/lib/auth";
 import type { APIRoute } from "astro";
 
-export const ALL = (async (ctx) => {
-  return await auth.handler(ctx.request);
+export const ALL = ((ctx) => {
+  return auth.handler(ctx.request);
 }) satisfies APIRoute;
